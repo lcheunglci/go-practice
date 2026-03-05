a89ba4c8af1a3ce4f65b9409347a7b9cabc6a7b6
Lawrence LCI <lcheunglci@gmail.com>
2026-03-04 23:08:32 -0800

    feat: use an array

--- demo/main.go
+++ demo/main.go
@@ -10,9 +10,16 @@ func main() {
 	// name := "Doe, Bob"
 	// var score = 87
 	// score := 87
-	name, score := "Doe, Bob", 87
+	// name, score := "Doe, Bob", 87
+	students := []string{"Doe, Bob",
+	   "Jones, Jess",
+	   "Wonders, Alice",
+	}
+	scores := []int{87, 96, 64}
 
 	fmt.Println("Student scores")
 	fmt.Println(strings.Repeat("-", 14))
-	fmt.Println(name, score)
+	fmt.Println(students[0], scores[0])
+	fmt.Println(students[1], scores[1])
+	fmt.Println(students[2], scores[2])
 }
