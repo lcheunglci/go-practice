7c469e4091acd72fea81d1b393bc7c67b89ce63a
Lawrence LCI <lcheunglci@gmail.com>
2026-03-08 23:52:40 -0700

    feat: add input in infinite loop to enter score

--- demo/main.go
+++ demo/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"strconv"
 )
 
 func main() {
@@ -11,55 +11,40 @@ func main() {
 		name string
 		score int 
 	}
+	
+	scores := []score {}
 
-	// var name string = "Doe, Bob"
-	// name := "Doe, Bob"
-	// var score = 87
-	// score := 87
-	// name, score := "Doe, Bob", 87
-	// students := []string{
-	// 	 "Doe, Bob",
-	//    "Jones, Jess",
-	//    "Wonders, Alice",
-	// }
-	// scores := []int{87, 96, 64}
-	// scores := map[string]int {
-	// 	students[0]: 87,
-	// 	students[1]: 96,
-	// 	students[2]: 64,
-
-	// }
-	scores := []score {
-
-		 {name: "Doe, Bob", score: 87},
-	   {name: "Jones, Jess", score: 96},
-	   {name: "Wonders, Alice", score: 64},
-
+	for {
+		fmt.Println("Enter a student name and score")
+		var name, rawScore string;
+		fmt.Scanln(&name, &rawScore)
+		s, _ := strconv.Atoi(rawScore)
+		scores = append(scores, score{name: name, score: s})
 	}
 
-	fmt.Println("Select score to print (1 - 3):")
-	var option string
-	fmt.Scanln(&option)
+	// fmt.Println("Select score to print (1 - 3):")
+	// var option string
+	// fmt.Scanln(&option)
 
-	fmt.Println("Student scores")
-	fmt.Println(strings.Repeat("-", 14))
-	// fmt.Println(students[0], scores[students[0]])
-	// fmt.Println(students[1], scores[students[1]])
-	// fmt.Println(students[2], scores[students[2]])
+	// fmt.Println("Student scores")
+	// fmt.Println(strings.Repeat("-", 14))
+	// // fmt.Println(students[0], scores[students[0]])
+	// // fmt.Println(students[1], scores[students[1]])
+	// // fmt.Println(students[2], scores[students[2]])
 
-	var index int 
-	switch option {
-  case "1": // should use strconv package in production
-		index = 0
-	case "2":
-		index = 1
-	case "3":
-		index = 2
-	default:
-		fmt.Println("Unknown option, defaulting to 1")
-		index = 0
-	}
-	fmt.Println(scores[index].name, scores[index].score)
+	// var index int 
+	// switch option {
+  // case "1": // should use strconv package in production
+	// 	index = 0
+	// case "2":
+	// 	index = 1
+	// case "3":
+	// 	index = 2
+	// default:
+	// 	fmt.Println("Unknown option, defaulting to 1")
+	// 	index = 0
+	// }
+	// fmt.Println(scores[index].name, scores[index].score)
 	// fmt.Println(scores[0].name, scores[0].score)
 	// fmt.Println(scores[1].name, scores[1].score)
 	// fmt.Println(scores[2].name, scores[2].score)
