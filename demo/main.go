e13d549ab7198621ff1b428269ddf93b0e4a7efa
Lawrence LCI <lcheunglci@gmail.com>
2026-03-08 23:59:16 -0700

    feat: add options

--- demo/main.go
+++ demo/main.go
@@ -3,49 +3,46 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 func main() {
 
 	type score struct {
-		name string
-		score int 
-	}
-	
-	scores := []score {}
-
-	for {
-		fmt.Println("Enter a student name and score")
-		var name, rawScore string;
-		fmt.Scanln(&name, &rawScore)
-		s, _ := strconv.Atoi(rawScore)
-		scores = append(scores, score{name: name, score: s})
+		name  string
+		score int
 	}
 
-	// fmt.Println("Select score to print (1 - 3):")
-	// var option string
-	// fmt.Scanln(&option)
+	scores := []score{}
+	shouldContinue := true
 
-	// fmt.Println("Student scores")
-	// fmt.Println(strings.Repeat("-", 14))
-	// // fmt.Println(students[0], scores[students[0]])
-	// // fmt.Println(students[1], scores[students[1]])
-	// // fmt.Println(students[2], scores[students[2]])
+	for shouldContinue == true {
+		fmt.Println("1) Enter a score")
+		fmt.Println("2) Print report")
+		fmt.Println("q) Quit")
+		fmt.Println()
+		fmt.Println("Please select an option")
 
-	// var index int 
-	// switch option {
-  // case "1": // should use strconv package in production
-	// 	index = 0
-	// case "2":
-	// 	index = 1
-	// case "3":
-	// 	index = 2
-	// default:
-	// 	fmt.Println("Unknown option, defaulting to 1")
-	// 	index = 0
-	// }
-	// fmt.Println(scores[index].name, scores[index].score)
-	// fmt.Println(scores[0].name, scores[0].score)
-	// fmt.Println(scores[1].name, scores[1].score)
-	// fmt.Println(scores[2].name, scores[2].score)
+		var option string
+		fmt.Scanln(&option)
+
+		switch option {
+		case "1":
+			fmt.Println("Enter a student name and score")
+			var name, rawScore string
+			fmt.Scanln(&name, &rawScore)
+			s, _ := strconv.Atoi(rawScore)
+			scores = append(scores, score{name: name, score: s})
+
+		case "2":
+			fmt.Println("Student scores")
+			fmt.Println(strings.Repeat("-", 14))
+			fmt.Println(scores)
+
+		case "q":
+			//tell the loop to stop
+			shouldContinue = false
+		}
+	}
+
 }
