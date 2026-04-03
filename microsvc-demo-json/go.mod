2684f26fbbecafe37dd4b0a730bf4da7faf082e9
Lawrence LCI <lcheunglci@gmail.com>
2026-04-02 20:49:33 -0700

    feat: add json example

---
+++ microsvc-demo-json/go.mod
@@ -0,0 +1,3 @@
+module microsvc-demo-json
+
+go 1.25.0
---
+++ microsvc-demo-json/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+func main() {
+
+	s := http.Server{
+		Addr: ":3000",
+	}
+
+	go func() {
+		log.Fatal(s.ListenAndServe())
+	}()
+
+	fmt.Println("Server started, press <Enter> to shutdown")
+	fmt.Scanln()
+	s.Shutdown(context.Background())
+	fmt.Println("Server stopped")
+
+}
+
+type Customer struct {
+	ID        int
+	FirstName string
+	LastName  string
+	Address   string
+}
+
+func readCustomers() ([]Customer, error) {
+	f, err := os.Open("customers.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	customers := make([]Customer, 0)
+	csvReader := csv.NewReader(f)
+	csvReader.Read() // throw away header
+	for {
+		fields, err := csvReader.Read()
+		if err == io.EOF {
+			return customers, nil
+		}
+		if err != nil {
+			return nil, err
+		}
+		var c Customer
+		id, err := strconv.Atoi(fields[0])
+		if err != nil {
+			continue
+		}
+		c.ID = id
+		c.FirstName = fields[1]
+		c.LastName = fields[2]
+		c.Address = fields[3]
+		customers = append(customers, c)
+	}
+}
