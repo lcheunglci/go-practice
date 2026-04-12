6fedf9780c90fa01f582e05b4b4b87f7ef36c444
Lawrence LCI <lcheunglci@gmail.com>
2026-04-11 23:55:58 -0700

    feat: add the tls config to use the cert

--- secure-comm-demo-certs/main.go
+++ secure-comm-demo-certs/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"crypto/tls"
 	"encoding/json"
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 type Payment struct {
@@ -13,11 +15,37 @@ type Payment struct {
 }
 
 func main() {
+
+	certFile := filepath.Join("certs", "server.crt")
+	keyFile := filepath.Join("certs", "server.key")
+
+	tlsConfig := &tls.Config{
+		MinVersion: tls.VersionTLS13,
+		CipherSuites: []uint16{
+			tls.TLS_AES_256_GCM_SHA384,
+			tls.TLS_AES_128_GCM_SHA256,
+			tls.TLS_CHACHA20_POLY1305_SHA256,
+		},
+		GetCertificate: func(info *tls.ClientHelloInfo) (*tls.Certificate, error) {
+			cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+			if err != nil {
+				log.Printf("Failed to load certificate: %v", err)
+				return nil, err
+			}
+			return &cert, nil
+		},
+	}
+
 	http.HandleFunc("GET /payments", handlePayments)
 	http.HandleFunc("GET /health", handleHealth)
 
-	log.Println("AtlasPay API starting on :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	server := &http.Server{
+		Addr:      ":8443",
+		TLSConfig: tlsConfig,
+	}
+
+	log.Println("AtlasPay API starting on :8443 with TLS 1.3")
+	log.Fatal(server.ListenAndServeTLS("", ""))
 }
 
 func handlePayments(w http.ResponseWriter, r *http.Request) {
